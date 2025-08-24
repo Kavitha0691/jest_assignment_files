df277d68088bb27046f587b4218b52cb2694cfb5
kavitha0691 <kavitha0691@gmail.com>
2025-08-24 18:00:55 +0200

    added css

--- src/component/Result/index.tsx
+++ src/component/Result/index.tsx
@@ -3,9 +3,9 @@ import { IResult } from "@/interfaces/interfaces";
 
 const Result: React.FC<IResult> = ({ rightAnswers, amountOfAnswers, onClick }) => {
   return (
-    <div data-testid="result">
-      <h2>{`${rightAnswers} of ${amountOfAnswers} right answers!`}</h2>
-      <button onClick={onClick}>Play again</button>
+    <div data-testid="result" className= "p-8 max-w-2xs mx-auto text-center">
+      <h2 className="text-xl font-bold text-gray-800 mb-6">{`${rightAnswers} of ${amountOfAnswers} right answers!`}</h2>
+      <button onClick={onClick} className="w-full  py-2 px-4 text-black bg-green-300 rounded-lg hover:bg-green-700 transition">Play again</button>
     </div>
   );
 };
